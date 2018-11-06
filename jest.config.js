338fb417b12ef283780d1037106f5605adbd77c7
Frederic Barthelemy <git@fbartho.com>
2018-11-06 11:59:39 -0800

    Eject from create-react-app

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,36 @@
-const jestPreset = require("babel-preset-jest");
 module.exports = {
   verbose: true,
-  ...jestPreset,
-  snapshotResolver: "<rootDir>/scripts/snapshotResolver.js"
+  snapshotResolver: "<rootDir>/scripts/snapshotResolver.js",
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+  resolver: "jest-pnp-resolver",
+  setupFiles: ["react-app-polyfill/jsdom"],
+  testMatch: ["<rootDir>/dist/**/__tests__/**/*.{js,jsx,ts,tsx}"],
+  testEnvironment: "jsdom",
+  testURL: "http://localhost",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)":
+      "<rootDir>/config/jest/fileTransform.js"
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
+  moduleNameMapper: {
+    "^react-native$": "react-native-web",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+  },
+  moduleFileExtensions: [
+    "web.js",
+    "js",
+    "web.ts",
+    "ts",
+    "web.tsx",
+    "tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node"
+  ]
 };
